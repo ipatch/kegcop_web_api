81323a6ef16f83483e162a8df0f33069c2ca53ad
ipatch <chris.r.jones.1983@gmail.com>
2014-08-14 18:56:27 -0500

    app should work in production environment now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # change the below line from 'false' to 'true' to get assets to display
+  # see this SO thread => http://stackoverflow.com/questions/18700219
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
