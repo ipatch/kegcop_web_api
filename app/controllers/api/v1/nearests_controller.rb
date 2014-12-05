01175559e8b84ffc9c314f9ebbc3b46c4d949754
ipatch <chris.r.jones.1983@gmail.com>
2014-12-04 18:18:51 -0600

    tshoot

--- app/controllers/api/v1/nearests_controller.rb
+++ app/controllers/api/v1/nearests_controller.rb
@@ -1,17 +1,17 @@
 # app/controllers/api/v1/events/nearests_controller.rb
 
-class Api::V1::Events::NearestsController < ApiController
-  def index
-    @events = Event.near(
-      [params[:lat], params[:lon]],
-      params[:radius],
-      units: :km
-    )
+# class Api::V1::Events::NearestsController < ApiController
+#   def index
+#     @events = Event.near(
+#       [params[:lat], params[:lon]],
+#       params[:radius],
+#       units: :km
+#     )
 
-    if @events.count(:all) > 0
-    	render
-    else
-    	render json: { message: 'No Events Found' }, status: 200
-    end
-  end
-end
+#     if @events.count(:all) > 0
+#     	render
+#     else
+#     	render json: { message: 'No Events Found' }, status: 200
+#     end
+#   end
+# end
