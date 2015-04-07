a741b0b9c79a614c1dfc390a24b6a59719931ec3
ipatch <chris.r.jones.1983@gmail.com>
2015-04-06 20:51:33 -0500

    add api key migration and model

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,5 +1,8 @@
 class API::V1::AccountsController < ApplicationController
 
+	# http_basic_authenticate_with name: "foo", password: "bar"
+	before_filter :restrict_access
+
 	def index
 		@accounts = Account.all # model names are singular :P
 		respond_to do |format|
@@ -7,5 +10,11 @@ class API::V1::AccountsController < ApplicationController
 		end
 	end
 
+	private
 
+		def restrict_access
+  			authenticate_or_request_with_http_token do |token, options|
+    			ApiKey.exists?(access_token: token)
+  		end
+  	end
 end
