1abc9f9aad026913ec64f13ff9a7f2a07a2044ce
ipatch <chris.r.jones.1983@gmail.com>
2015-04-08 13:38:22 -0500

    add directory to puma config

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,7 @@
 # The default is the current directory.
 #
 # directory '/u/apps/lolcat'
+directory '/opt/rails/kegcop'
 
 # Use an object or block as the rack application. This allows the
 # config file to be the application itself.
