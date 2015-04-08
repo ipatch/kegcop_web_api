5ed47f486111ac27ac1ca5fa3e78e0aac996dabc
ipatch <chris.r.jones.1983@gmail.com>
2015-04-08 14:47:00 -0500

    remove version number from API URL using a constraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-# require 'api_constraints'
+require 'api_constraints'
 
 Rails.application.routes.draw do
  # Api definition
   namespace :api, defaults: {format: 'json'} do
     # use scpoe to remove the version number from the API when making a request
-    # scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-    namespace :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    # namespace :v1 do
       resources :accounts
       resources :please_work
     end
--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -1,6 +1,3 @@
-# this file is going to need modification
-
-
 class ApiConstraints
   def initialize(options)
     @version = options[:version]
@@ -8,8 +5,7 @@ class ApiConstraints
   end
 
   def matches?(req)
-  	# vnd.marketplace. <= is not going to work :'(
-    # @default || req.headers['Accept'].include?("application/vnd.api.kegcop.chrisrjones.v#{@version}")
+    @default || req.headers['Accept'].include?("application/vnd.kegcop.chrisrjones.v#{@version}")
   	return true
 
   end
