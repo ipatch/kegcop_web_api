32e5ff6b8e5810016048375af2f619fa646e6635
ipatch <chris.r.jones.1983@gmail.com>
2015-04-09 17:46:35 -0500

    update gitignore

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -14,7 +14,7 @@ class API::V1::AccountsController < ApplicationController
 
 		def restrict_access
   			authenticate_or_request_with_http_token do |token, options|
-    			ApiKey.exists?(access_token: token)
+    			APIKey.exists?(access_token: token)
   		end
   	end
 end
