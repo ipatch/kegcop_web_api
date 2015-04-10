960575c9c21adf70d55835f671b41e15c5d9928b
ipatch <chris.r.jones.1983@gmail.com>
2015-04-10 02:02:51 -0500

    fix typo in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     # namespace :v1 do
       resources :accounts
       resources :please_work
-      resources :kegerators, param :device_id
+      resources :kegerators, param: :device_id
     end
   end
 
