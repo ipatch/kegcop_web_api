0d49a9c40d652cf2dbd9dc61694403a8c17ab554
ipatch <chris.r.jones.1983@gmail.com>
2015-10-29 23:21:49 -0500

    add secret key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,7 @@ set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 # files we want symlinking to specific entries in shared.
 set :linked_files,    %w{config/database.yml config/secrets.yml}
-# set :puma_bind,       'tcp://0.0.0.0:9292' 
+set :puma_bind,       'tcp://0.0.0.0:9292' 
 set :puma_bind,       'unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock'
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
