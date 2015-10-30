f33a76cb35a04979066418023e43ae3dbdf7da98
ipatch <chris.r.jones.1983@gmail.com>
2015-10-29 19:44:55 -0500

    tshoot puma

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,8 @@ set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 # files we want symlinking to specific entries in shared.
 set :linked_files,    %w{config/database.yml}
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       %w(tcp://0.0.0.0:9292 unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock)
+set 
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
