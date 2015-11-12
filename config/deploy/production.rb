4cdef145938f60e22be58f8f23f8e1b939dbdb21
ipatch <chris.r.jones.1983@gmail.com>
2015-11-12 14:37:58 -0600

    work on trying to preserve uploaded files through deployments with cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,6 +21,7 @@ set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 # files we want symlinking to specific entries in shared.
 set :linked_files,    %w{config/database.yml config/secrets.yml}
+# see this SO answer, http://stackoverflow.com/a/32011351/708807
 set :linked_dirs, fetch(:linked_dirs, []).push('public/uploads')
 set :puma_bind,       'tcp://0.0.0.0:9292' 
 set :puma_bind,       'unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock'
