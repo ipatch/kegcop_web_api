9317383b4caa2f533205d02178dbf4be0482ce2c
ipatch <chris.r.jones.1983@gmail.com>
2015-11-11 20:44:17 -0600

    try wildcard in nginx conf

--- app/controllers/api/v1/csv_files_controller.rb
+++ app/controllers/api/v1/csv_files_controller.rb
@@ -14,16 +14,8 @@ class API::V1::CsvFilesController < ApplicationController
 
   def show
     @csv_file = CsvFile.find(params[:id])
-
-    # respond_to do |format|
-
       redirect_to CsvFile.find(params[:id]).csv_file_url
-      # format.html
-      # this function should return json
-      # { url: csv_file_url }
-      # render json: 
       #render json: CsvFile.find(params[:id]).csv_file_url
-    # end
   end
 
   # POST /csv_files.json
