98809ddaf7ad760f50ddcdb81cf147332f7a7a62
ipatch <chris.r.jones.1983@gmail.com>
2017-04-28 17:16:58 -0500

    edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -141,13 +141,13 @@ set :ssh_options, {
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
+server '107.170.40.252',
+  user: 'deploy',
+  roles: %w{web app},
+  ssh_options: {
+    user: 'deploy', # overrides user setting above
+    keys: %w(/home/user_name/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey password)
 #     # password: 'please use keys'
-#   }
+  }
