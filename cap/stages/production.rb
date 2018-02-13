badab5703aa9195f6a5c54a08411653382c1a761
ipatch <chris.r.jones.1983@gmail.com>
2018-02-13 00:12:21 -0600

    I was in a bar and I walked up to a beautiful woman and said, "Do you live around here often?"  She said, "You're wearing two different-color socks." I said, "Yes, but to me they're the same because I go by thickness." She said, "How do you feel?" And I said, "You know when you're sitting on a chair and you lean back so you're just on two legs and you lean too far so you almost fall over but at the last second you catch yourself?  I feel like that all the time..." 		-- Steven Wright, "Gentlemen's Quarterly"

--- cap/stages/production.rb
+++ cap/stages/production.rb
@@ -6,7 +6,7 @@
 server '107.170.40.252', port: 4321, user: 'deploy', roles: %w{web app db}, primary: true
 
 set :ssh_options, {
-  keys: %w(/home/deploy/.ssh/id_rsa),
+  keys: %w($HOME/.ssh/id_rsa),
   forward_agent: true,
   auth_methods: %w(publickey),
   user: 'deploy',
