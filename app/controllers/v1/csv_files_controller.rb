56c1005384571c5900992b2d53003efc07a0e9b4
ipatch <chris.r.jones.1983@gmail.com>
2018-02-19 16:49:39 -0600

    add a couple of concerns for working with the API

--- app/controllers/v1/csv_files_controller.rb
+++ app/controllers/v1/csv_files_controller.rb
@@ -6,13 +6,14 @@ module V1
 
     respond_to :html, :json
     
+    # GET / csv_files
     def index
       @csv_files = CsvFile.all
       # respond_with(@csv_files)
       json_response(@csv_files)
     end
 
-    # POST /csv_files.json
+    # POST /csv_files
     def create
       csv_file = CsvFile.new
       csv_file.csv_file = params[:csv_file]
@@ -20,11 +21,36 @@ module V1
       render :text => "Ok"
     end
 
-    def json_response(object, status = :ok)
-      render json: object, status: status
+    # GET /csv_files/:id
+    def show
+      json_response(@csv_file)
     end
 
+    # PUT /csv_files/:id
+    def update
+      @csv_file.update(csv_file_params)
+      head :no_content
+    end
+
+    # DELETE /csv_files/:id
+    def destroy
+      @csv_file.destroy
+      head :no_content
+    end
+
+    # def json_response(object, status = :ok)
+    #   render json: object, status: status
+    # end
+
     private
+      def csv_file_params
+        # whitelist params
+        params.permit()
+      end
+
+      def set_csv_file
+        @csv_file = CsvFile.find(params[:id])
+      end
       # def csv_file_params
       #   params.permit(:csv_file)
       # end
