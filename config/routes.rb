1d0eb510b9264c73b7e815e8966a6687131e3249
ipatch <chris.r.jones.1983@gmail.com>
2018-02-18 23:56:58 -0600

    remove eronous routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
-require 'api_constraints'
+require 'api_version'
 
 Rails.application.routes.draw do
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: 
+  #...first created -> highest priority.
   # `rake routes`
 
   # Api definition
@@ -10,27 +11,8 @@ Rails.application.routes.draw do
   # namespace :api, defaults: {format: 'json'} do
     # use scpoe to remove the version number from the API when making a request
   scope module: :v1, constraints: ApiVersion.new('v1',true) do
-    resources :accounts
-    resources :please_work
-    resources :kegerators, param: :device_id
-    resources :register
     resources :csv_files
   end
-
-  # shortened routes, per railscast comment
-  get 'signup' => 'users#new'
-  get 'login' => 'sessions#new'
-  get 'logout' => 'sessions#destroy'
-
-  # add routes for static pages
-  get 'demo' => 'static_pages#demo'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-
-  resources :users
-  resources :sessions
-  resources :static_pages
-  resources :bubbles
 
   # root to: 'users#new'
   root to: 'bubbles#index'
