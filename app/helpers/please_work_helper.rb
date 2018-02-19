de57366b994be96ce5f1b63582f3e19339854423
ipatch <chris.r.jones.1983@gmail.com>
2018-02-18 23:51:34 -0600

    delete a bunch of files

--- app/helpers/please_work_helper.rb
+++
@@ -1,2 +0,0 @@
-module PleaseWorkHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,28 +3,18 @@ require 'api_constraints'
 Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # get  'csvfiles'    => 'application#index'
+  # `rake routes`
 
   # Api definition
     # the below line specifies JSON as the default API format
-  namespace :api, defaults: {format: 'json'} do
+  # namespace :api, defaults: {format: 'json'} do
     # use scpoe to remove the version number from the API when making a request
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-    # namespace :v1 do
-      resources :accounts
-      resources :please_work
-      resources :kegerators, param: :device_id
-      resources :register
-      resources :csv_files do
-        # match "/upload" => "csv_files#upload", :as => :upload
-        # post 'upload', to: 'csv_files#create'
-      end
-      # map.upload "/upload(.:format)", :controller => "csv_files", :action => "upload"
-      # match ':controller/:action/:id', via: [:post]
-      
-    end
+  scope module: :v1, constraints: ApiVersion.new('v1',true) do
+    resources :accounts
+    resources :please_work
+    resources :kegerators, param: :device_id
+    resources :register
+    resources :csv_files
   end
 
   # shortened routes, per railscast comment
