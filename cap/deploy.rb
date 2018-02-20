90c769ddd9224bdee9b4f64d20ae4bea4620cb00
ipatch <chris.r.jones.1983@gmail.com>
2018-02-20 14:50:28 -0600

    The means-and-ends moralists, or non-doers, always end up on their ends without any means. 		-- Saul Alinsky

--- cap/deploy.rb
+++ cap/deploy.rb
@@ -14,9 +14,8 @@ set :repo_url,        'git@github.com:ipatch/kegcop_web_api'
 set :rvm_ruby_version, '2.3.1'
 # TODO: see if `$PATH` env var can be used instead of
 #...explicitly defining all the paths.
-# set :default_env, { path: 
-# "$HOME/.rvm/bin:/usr/local/bin:/usr/bin:/bin:$HOME/.asdf/bin:$HOME/.asdf/shims" }
-set :default_env, { 'PATH' => $PATH }
+# NOTE: cap can't read env vars such `$PATH` from the local user 😡
+set :default_env, { path: "$HOME/.rvm/bin:/usr/local/bin:/usr/bin:/bin:$HOME/.asdf/bin:$HOME/.asdf/shims" }
 set :bundle_flags, '--deployment'
 set :rvm_roles, [:app, :web, :db]
 SSHKit.config.command_map[:rake] = "#{fetch(:default_env)[:rvm_bin_path]}/rvm ruby-#{fetch(:rvm_ruby_version)} do bundle exec rake"
